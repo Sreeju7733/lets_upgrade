d7d7f6c4b30490b7e3542b20205212d09690150e
Sreeju <77568405+Sreeju7733@users.noreply.github.com>
2023-05-21 16:47:29 +0530

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,6 @@
+function sum(num1, num2){
+    return num1 + num2
+}
+var num1 = Number(prompt("Enter first number: "))
+var num2 = Number(prompt("Enter second number: "))
+console.log(sum(num1, num2))
